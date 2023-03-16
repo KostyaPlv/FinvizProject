0c44fa2e2aa5429cbe5e0699a05f298afa928430
KostyaPlv <46071234+KostyaPlv@users.noreply.github.com>
2023-03-16 09:27:13 +0200

    Update PortfolioTest.java

--- src/test/java/testsPackage/PortfolioTest.java
+++ src/test/java/testsPackage/PortfolioTest.java
@@ -1,16 +1,14 @@
 package testsPackage;
 
 import static org.testng.Assert.assertEquals;
-
 import org.testng.annotations.Test;
-
 import pageObjects.HomePage;
 import pageObjects.LoginPage;
 import pageObjects.PortfolioPage;
 
 public class PortfolioTest extends BaseTest {
 
-	@Test
+	@Test(description = "Creating new portfolio")
 	public void tc_07_createNewPortfolio() {
 		PortfolioPage pop = new PortfolioPage(driver);
 		LoginPage lp = new LoginPage(driver);
@@ -22,19 +20,19 @@ public class PortfolioTest extends BaseTest {
 		pop.todayPriceBtn();
 	}
 
-	@Test
+	@Test(description = "Fill current price button")
 	public void tc_08_fillLine2() {
 		PortfolioPage pop = new PortfolioPage(driver);
 		pop.fillForm2("teva", "Short Sell", "10-22-2014", "10", "300");
 	}
 
-	@Test
+	@Test(description = "Add more rows button")
 	public void tc_09_addRowsButton() {
 		PortfolioPage pop = new PortfolioPage(driver);
 		pop.moreRowsBtn();
 	}
 
-	@Test
+	@Test(description = "Save the portfolio button")
 	public void tc_10_saveButton() {
 		PortfolioPage pop = new PortfolioPage(driver);
 		pop.saveChangesBtn();
@@ -44,7 +42,7 @@ public class PortfolioTest extends BaseTest {
 		assertEquals(actual, expected);
 	}
 
-	@Test
+	@Test(description = "Delete the portfolio button")
 	public void tc_11_deleteButton() {
 		PortfolioPage pop = new PortfolioPage(driver);
 		pop.deletePortfolioBtn();
